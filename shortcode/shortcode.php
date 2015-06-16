31fb2bf5e180fa90a85b8d88d5975dbfbe384f97
mthjn <jana.mathauserova@ubk.cz>
2015-06-16 11:02:18 +0200

    it works

--- shortcode/shortcode.php
+++ shortcode/shortcode.php
@@ -2,19 +2,36 @@
 /**
  * @package     Shortcode
  * @subpackage  plg_shortcode
- *
  * @license     GNU General Public License version 2 or later.
+ * More
+ * @link http://cocoate.com/jdev/plugin
+ * @link http://learnwebtutorials.com/tutorial-creating-joomla-3-plugin
  */
 
 defined('_JEXEC') or die;
+
+//jimport('joomla.plugin.plugin');
 
 class plgContentShortcode extends JPlugin
 {
   protected $autoloadLanguage = true;
 
-  function onContentPrepare( $context, $article, $params, $limitstart )
+  function onContentPrepare( $context, &$article, &$params, $limitstart=0 )
   {
-    $article->text = str_replace('[bild]', 'This replaces the shortcode', $article->text);
+/*
+    preg_match_all('/{bild url="' . $params->get('url') . '"}(.*?){\/bild}/is', $article->text, $matches);
+    $i = 0;
+    foreach ($matches[0] as $match) {
+     $done = $matches[1][$i];
+     $article->text = str_replace($match, $done, $article->text);
+    }
+*/
+
+  // Simple
+  $shortcode = '[bild]';
+  $sh_content = 'This replaces a shortcode';
+  $article->text = str_replace( $shortcode, $sh_content, $article->text);
+
       return true;
   }
 
