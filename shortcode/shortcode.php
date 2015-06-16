fbe3e6dce72e011f30daad7925181c880a7526cd
mthjn <jana.mathauserova@ubk.cz>
2015-06-16 15:48:53 +0200

    bug fix

--- shortcode/shortcode.php
+++ shortcode/shortcode.php
@@ -20,7 +20,7 @@
 
      ob_start();?>
      <style type="text/css">
-     .clearfix {content:"";display:table;clear:both}.sh-bild-img{width:100%;transition: all .2s ease-in-out}.sh-bild:hover .sh-bild-img{transform:scale(1.2)}.sh-bild-btns a{display:inline-block;padding:7px 12px;margin:3px;font-size:12px;letter-spacing:2px;line-height:1;text-align:center;vertical-align:middle;cursor:pointer;background:transparent;color:#fff;border:1px solid #fff;border-radius:100px;text-decoration:none;text-transform:uppercase;-webkit-transform: scale3d(0, 0, 0);transform: scale3d(0, 0, 0);-webkit-transition: all 400ms;transition: all 400ms;}
+     .clearfix {content:"";clear:both}.sh-bild-img{width:100%;transition: all .2s ease-in-out}.sh-bild:hover .sh-bild-img{transform:scale(1.2)}.sh-bild-btns a{display:inline-block;padding:7px 12px;margin:3px;font-size:12px;letter-spacing:2px;line-height:1;text-align:center;vertical-align:middle;cursor:pointer;background:transparent;color:#fff;border:1px solid #fff;border-radius:100px;text-decoration:none;text-transform:uppercase;-webkit-transform: scale3d(0, 0, 0);transform: scale3d(0, 0, 0);-webkit-transition: all 400ms;transition: all 400ms;}
      .sh-bild-btns a:hover{background:#000;border:1px solid black}
      .sh-bild-btns:hover a{-webkit-transform: scale3d(0, 0, 0); transform: scale3d(1,1,1); -webkit-transition: all 400ms; transition: all 400ms;}
      .sh-bild-btns h4{opacity:0;color:#fff;-webkit-transform: translate3d(0, 15px, 0);transform: translate3d(0, 15px, 0);-webkit-transition: all 400ms;transition: all 400ms;}
